467b35bef670e7903910eecf37994eaa22e28e1b
Umut Aslan <58588088+umutaslan1@users.noreply.github.com>
2023-11-06 06:34:24 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Anasayfa bağlantısını seçin
-var homeLink = document.getElementById('homeLink');
-
-// Tıklanabilirlik işlevini ekleyin
-homeLink.addEventListener('click', function(event) {
-    event.preventDefault(); // Varsayılan tıklama davranışını engelle
-    window.location.href = 'index.html'; // Anasayfaya yönlendir
-});
